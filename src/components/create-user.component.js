ccccab33f42c15c332b0de8564f73af7ef41bb9b
Pranavi <pranavivelisoju@gmail.com>
2020-01-08 04:33:49 +0530

    Exercise list is as homepage , we can edit and delete . and those are connected to db. performing crud operations

--- src/components/create-user.component.js
+++ src/components/create-user.component.js
@@ -25,6 +25,7 @@ export default class CreateUser extends Component {
     };
 
     console.log(newUser);
+
     axios
       .post("http://localhost:5000/users/add", newUser)
       .then(res => console.log(res.data));
